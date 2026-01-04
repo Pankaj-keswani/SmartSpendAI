7fdee1956201e47467f8217f3dba8a772a058428
Pankaj Keswani <pankajkeswani111@gmail.com>
2026-01-04 21:06:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,12 @@ def analyze():
     
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
 
 
 if __name__ == "__main__":
